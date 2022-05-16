048743d7f7a9536ecc30b92f16560fd05a9f8a93
JackTabronEXE <89229461+JackTabronEXE@users.noreply.github.com>
2022-05-16 13:03:44 +1200

    Inflate the unicorn solved

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,23 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+var counter = 1
 
-console.log("Inflate The Unicorn!")
+imgClickAndChange.onclick = function () {
+  if (counter == 0) {
+    document.getElementById('imgClickAndChange').src = './images/unicorn-0.png'
+    counter++
+  } else if (counter == 1) {
+    document.getElementById('imgClickAndChange').src = './images/unicorn-1.png'
+    counter++
+  } else if (counter == 2) {
+    document.getElementById('imgClickAndChange').src = './images/unicorn-2.png'
+    counter++
+  } else if (counter == 3) {
+    document.getElementById('imgClickAndChange').src = './images/unicorn-3.png'
+    counter++
+  } else if (counter == 4) {
+    window.alert('Congratulations on inflating a unicorn')
+    counter = 0
+  }
+}
