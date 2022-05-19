a201483b2311d47bfe23dfbc58ccbd046049abc7
JackTabronEXE <89229461+JackTabronEXE@users.noreply.github.com>
2022-05-19 13:58:43 +1200

    solved problems

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,102 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+let headImg = './images/head'
+let bodyImg = './images/body'
+let shoesImg = './images/shoes'
+let head = document.getElementById(`head`)
+let body = document.getElementById(`body`)
+let shoes = document.getElementById(`shoes`)
+
+let headIndex = 0
+let bodyIndex = 0
+let shoesIndex = 0
+let clothesIndex = 0
+
+function indexUp() {
+  if (clothesIndex < 2) {
+    clothesIndex++
+  } else {
+    clothesIndex = 0
+  }
+}
+
+function indexDown() {
+  if (clothesIndex <= 0) {
+    clothesIndex = 2
+  } else if (clothesIndex <= 2) {
+    clothesIndex--
+  }
+}
+
+function indexRight() {
+  if (clothesIndex == 0) {
+    head.src = headImg + headIndex + '.png'
+    if (headIndex < 5) {
+      headIndex++
+    } else {
+      headIndex = 0
+    }
+  } else if (clothesIndex == 1) {
+    body.src = bodyImg + bodyIndex + '.png'
+    if (bodyIndex < 5) {
+      bodyIndex++
+    } else {
+      bodyIndex = 0
+    }
+  } else if (clothesIndex == 2) {
+    shoes.src = shoesImg + shoesIndex + '.png'
+    if (shoesIndex < 5) {
+      shoesIndex++
+    } else {
+      shoesIndex = 0
+    }
+  }
+}
+
+function indexLeft() {
+  if (clothesIndex == 0) {
+    head.src = headImg + headIndex + '.png'
+    if (headIndex <= 0) {
+      headIndex = 5
+    } else if (headIndex <= 5) {
+      headIndex--
+    }
+  } else if (clothesIndex == 1) {
+    body.src = bodyImg + bodyIndex + '.png'
+    if (bodyIndex <= 0) {
+      bodyIndex = 5
+    } else if (bodyIndex <= 5) {
+      bodyIndex--
+    }
+  } else if (clothesIndex == 2) {
+    shoes.src = shoesImg + shoesIndex + '.png'
+    if (shoesIndex <= 0) {
+      shoesIndex = 5
+    } else if (shoesIndex <= 5) {
+      shoesIndex--
+    }
+  }
+}
+
+document.onkeydown = checkKey
+
+function checkKey(e) {
+  e = e || window.event
+  switch (e.keyCode) {
+    case 37:
+      indexLeft()
+      break
+    case 38:
+      indexUp()
+      break
+    case 39:
+      indexRight()
+      break
+    case 40:
+      indexDown()
+      break
+  }
+}
